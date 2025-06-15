d43d59a5bf6e1739904d913d38967d80b8a00a54
Julia Azevedo Lins <julia.azevedolins@gmail.com>
2025-06-15 11:56:39 -0300

    Criando a DTO para cadastro de usuário

---
+++ src/main/java/com/challenge/investimentos/investimentos_api/dto/UsuarioCadastroDTO.java
@@ -0,0 +1,13 @@
+package com.challenge.investimentos.investimentos_api.dto;
+
+public class UsuarioCadastroDTO {
+    private String cpfIdentificacao;
+
+    public String getCpfIdentificacao() {
+        return cpfIdentificacao;
+    }
+
+    public void setCpfIdentificacao(String cpfIdentificacao) {
+        this.cpfIdentificacao = cpfIdentificacao;
+    }
+}
